bb8201df3123085ccbcb42a206b02e258fa37556
Pratik Prajapati <pratikprajapatisci@gmail.com>
2022-05-16 13:31:21 +0530

    Initial commit

---
+++ Softgames Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Softgames Test
+//
+//  Created by Pratikkumar Prajapati on 16/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
